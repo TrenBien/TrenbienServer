339ccf62b22add73666e0059d408a2425fd28c91
Ubuntu <ubuntu@ip-172-26-1-106.ap-northeast-2.compute.internal>
2022-12-14 01:44:50 +0900

    initial commit

---
+++ src/main/java/com/ybigta/trenbien/TrenbienApplication.java
@@ -0,0 +1,13 @@
+package com.ybigta.trenbien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrenbienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrenbienApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ybigta/trenbien/TrenbienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ybigta.trenbien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrenbienApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
